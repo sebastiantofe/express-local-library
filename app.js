9aeadc20131b9fc595ea1592b3ea0a6254e449a9
Sebastian Torrado <sebastiantofe@gmail.com>
2021-03-27 21:23:55 -0500

    Added pug template and models

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+
+const mongoDB = 'mongodb+srv://TofeAdmin:Sebastians@learning.op386.mongodb.net/local_library?retryWrites=true&w=majority'
+// const mongoDB = 'mongodb://TofeLibrary:sebastians@127.0.0.1:27017/localLibrary';
+
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+let db = mongoose.connection;
+
+
+
+
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +36,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
