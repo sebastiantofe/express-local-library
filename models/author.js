ac6f392a51e760c03e65889c80a2cfc5512fa4aa
Sebastian Torrado <sebastiantofe@gmail.com>
2021-04-02 22:24:54 -0500

    Added all list views, controllers

--- models/author.js
+++ models/author.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+const { DateTime } = require('luxon');
+
 const authorSchema = new Schema({
 	first_name: { type: String, required: true, maxlength: 100 },
 	family_name: { type: String, required: true, maxlength: 100 },
@@ -30,5 +32,16 @@ authorSchema
 	return '/catalog/author/' + this._id
 });
 
+authorSchema
+.virtual('date_of_birth_formatted')
+.get(function() {
+	return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+authorSchema
+.virtual('date_of_death_formatted')
+.get(function() {
+	return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+});
 
 module.exports = mongoose.model("Author", authorSchema);
--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const { Schema } = mongoose;
 
@@ -13,6 +14,12 @@ bookInstanceSchema
 .virtual('url')
 .get(function() {
 	return '/catalog/bookinstance/' + this._id 
+});
+
+bookInstanceSchema
+.virtual('due_back_formatted')
+.get(function() {
+	return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 module.exports = mongoose.model('BookInstance', bookInstanceSchema);
